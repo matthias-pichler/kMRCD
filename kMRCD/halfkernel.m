60832bdd512617738ce12e8d93390b3510b831d5
Matthias Pichler <e1634256@student.tuwien.ac.at>
2023-11-05 22:24:31 -0800

    feat: add more examples

--- kMRCD/halfkernel.m
+++ kMRCD/halfkernel.m
@@ -1,11 +1,11 @@
-function data = halfkernel(N1, N2, minx, r1, r2, noise, ratio)
+function data = halfkernel(numInliers, numOutliers, minx, r1, r2, noise, ratio)
 % HALFKERNEL generates a non-elliptical 2D dataset
-%  data = HALFKERNEL(N1, N2, minx, r1, r2, noise, ratio)
+%  data = HALFKERNEL(N1, numOutliers, minx, r1, r2, noise, ratio)
 %
 % Input
-%   N1 (1,1) double {mustBePositive, mustBeInteger}
+%   numInliers (1,1) double {mustBePositive, mustBeInteger}
 %       number of inliers
-%   N2 (1,1) double {mustBePositive, mustBeInteger}
+%   numOutliers (1,1) double {mustBePositive, mustBeInteger}
 %       number of outliers
 %   minx (1,1) double
 %       minimum x coordinate
@@ -19,7 +19,7 @@ function data = halfkernel(N1, N2, minx, r1, r2, noise, ratio)
 %       size ration between x and y span (ratio ~ (max(y)-min(y))/(max(x)-min(x))
 %
 % Output
-%   data (N1+N2,3) double
+%   data (N1+numOutliers,3) double
 %       data(:,1) - x coordinate
 %       data(:,2) - y coordinate
 %       data(:,3) - class label
@@ -27,8 +27,8 @@ function data = halfkernel(N1, N2, minx, r1, r2, noise, ratio)
 %       data(N1+1:end,3) = 0 outliers
 
 arguments
-    N1 (1,1) double {mustBePositive, mustBeInteger}
-    N2 (1,1) double {mustBePositive, mustBeInteger}
+    numInliers (1,1) double {mustBePositive, mustBeInteger}
+    numOutliers (1,1) double {mustBePositive, mustBeInteger}
     minx (1,1) double
     r1 (1,1) double {mustBePositive}
     r2 (1,1) double {mustBePositive}
@@ -36,11 +36,11 @@ arguments
     ratio (1,1) double {mustBeInRange(ratio, 0, 1)}
 end
 
-phi1 = rand(N1,1) * pi;
-inner = [minx + r1 * sin(phi1) - .5 * noise  + noise * rand(N1,1), r1 * ratio * cos(phi1) - .5 * noise + noise * rand(N1,1), ones(N1,1)];
+phi1 = rand(numInliers,1) * pi;
+inner = [minx + r1 * sin(phi1) - .5 * noise  + noise * rand(numInliers,1), r1 * ratio * cos(phi1) - .5 * noise + noise * rand(numInliers,1), ones(numInliers,1)];
 
-phi2 = rand(N2,1) * pi;
-outer = [minx + r2 * sin(phi2) - .5 * noise  + noise * rand(N2,1), r2 * ratio * cos(phi2) - .5 * noise  + noise * rand(N2,1), zeros(N2,1)];
+phi2 = rand(numOutliers,1) * pi;
+outer = [minx + r2 * sin(phi2) - .5 * noise  + noise * rand(numOutliers,1), r2 * ratio * cos(phi2) - .5 * noise  + noise * rand(numOutliers,1), zeros(numOutliers,1)];
 
 data = [inner; outer];
 end
