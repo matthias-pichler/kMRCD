70f1fa68e4de928c82966f2ea84e8d5c3f7aad93
Matthias Pichler <e1634256@student.tuwien.ac.at>
2023-11-28 16:30:22 -0800

    feat: add reuters21578

---
+++ thesis/reuters21578.m
@@ -0,0 +1,107 @@
+%% Reuters 21578
+% See https://faculty.cc.gatech.edu/~hpark/papers/textoutlier17.pdf
+
+% Setup
+clc;
+clear all;
+close all;
+rng(1634256, "twister");
+
+projectDir = fileparts(fileparts(which(mfilename)));
+
+modelName = 'all-mpnet-base-v2';
+% modelName = 'bge-large-en-v1.5';
+% modelName = 'all-MiniLM-L6-v2';
+datasetName = 'reuters21578';
+
+imageDir = fullfile(projectDir, 'images', datasetName);
+tableDir = fullfile(projectDir, 'tables', datasetName);
+datasetDir = fullfile(projectDir, 'datasets', datasetName);
+
+file = fullfile(datasetDir, [datasetName '_' modelName '.parquet']);
+
+mkdir(imageDir, modelName);
+mkdir(tableDir, modelName);
+
+%% Load Data
+
+rawData = parquetread(file, SelectedVariableNames=["text", "topics", "embedding"]);
+rawData.topics = cellfun(@categorical, rawData.topics, UniformOutput=false);
+
+inlierTopics = {'acq', 'earn'};
+
+inlierIndices = find(cellfun(@(t)any(ismember(inlierTopics, t)), rawData.topics));
+outlierIndices = find(cellfun(@(t)ismember("interest", t), rawData.topics));
+
+%inlierIndices = datasample(inlierIndices, 900, Replace=false);
+%outlierIndices = datasample(outlierIndices, 100, Replace=false);
+
+data = rawData(sort(cat(1, inlierIndices, outlierIndices)),:);
+outlierIndices = cellfun(@(t)ismember("interest", t), data.topics);
+    
+embeddings = cell2mat(cellfun(@transpose,data.embedding, UniformOutput=false));
+labels = categorical(repmat("inlier", height(data), 1), {'inlier' 'outlier'});
+labels(outlierIndices) = "outlier";
+
+perm = randperm(height(data));
+embeddings = embeddings(perm, :);
+labels = labels(perm, :);
+
+clear rawData inlierIndices outlierIndices perm;
+
+%% Visualize
+
+alpha = 0.7;
+
+Y = tsne(embeddings, Distance="cosine");
+fig = figure(1);
+gscatter(Y(:,1), Y(:,2), labels);
+title("t-SNE Embeddings");
+saveas(fig,fullfile(imageDir, modelName, "tsne.png"),'png');
+
+clear Y;
+
+%% Run
+
+kModel = AutoSphereRbfKernel(embeddings);
+% kModel = AutoRbfKernel(data);
+poc = kMRCD(kModel, Estimators={'SDO' 'SpatialMedian' 'SSCM'}); 
+solution = poc.runAlgorithm(embeddings, alpha);
+
+%% Evaluation
+
+% h Subset
+hSubset = table(labels(solution.hsubsetIndices), VariableNames="label");
+hSubsetSummary = groupcounts(hSubset, "label");
+writetable(hSubsetSummary, fullfile(tableDir, modelName, "e02_h_subset.csv"));
+
+clear hSubset hSubsetSummary;
+
+% Confusion Matrix
+grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
+grouphat(solution.flaggedOutlierIndices) = "outlier";
+
+cm = confusionmat(labels,grouphat);
+
+fig = figure(2);
+confusionchart(fig, cm, categories(labels));
+saveas(fig, fullfile(imageDir, modelName, 'confusion_matrix.png'),'png');
+
+clear cm grouphat;
+
+% Mahalanobis Distances
+fig = figure(3);
+mahalchart(labels, solution.rd, solution.cutoff);
+saveas(fig, fullfile(imageDir, modelName, 'mahalanobis_distances.png'),'png');
+
+% Comparison
+fig = figure(4);
+stats = evaluation(embeddings, labels, alpha, solution);
+saveas(fig, fullfile(imageDir, modelName, 'pr_curve.png'),'png');
+writetable(stats, fullfile(tableDir, modelName, "comparison.csv"));
+
+clear stats;
+
+clear solution kModel alpha poc;
+clear data labels embeddings;
+    
