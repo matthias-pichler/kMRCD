04828f085e80000ca93c5b59d63cec00171d8cdd
Matthias Pichler <e1634256@student.tuwien.ac.at>
2023-11-30 13:59:39 -0800

    only use lof & iforest as benchmark

--- evaluation/evaluation.m
+++ evaluation/evaluation.m
@@ -5,68 +5,93 @@ function result = evaluation(data, labels, alpha, solution, NameValueArgs)
         alpha (1,1) double {mustBeInRange(alpha,0.5,1)}
         solution struct
         NameValueArgs.CategoricalPredictors = []
+        NameValueArgs.Estimators {mustBeMember(NameValueArgs.Estimators, {'lof' 'iforest' 'rrcforest' 'ocsvm' 'robustcov'})} = {'lof' 'iforest' 'robustcov'}
     end
 
-    assert(isequal(size(labels), [size(data, 1), 1]))
+    assert(isequal(size(labels), [height(data), 1]))
     
     stats = struct();
     scores = struct();
 
+    % kMRCD
     grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
     grouphat(solution.flaggedOutlierIndices) = "outlier";
     stats.kMRCD = confusionstats(confusionmat(labels,grouphat, Order={'outlier' 'inlier'}));
     scores.kMRCD = solution.rd;
 
-    % TODO add scores for kMRCD
+    % MCD
+    if ismember('robustcov', NameValueArgs.Estimators)
+        tic;
+        [~, ~, sc, tf] = robustcov(data, OutlierFraction=(1-alpha));
+        t = toc;
+        fprintf("robustcov: %0.4f sec\n", t);
 
+        grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
+        grouphat(tf) = "outlier";
+        stats.robustcov = confusionstats(confusionmat(labels,grouphat, Order={'outlier' 'inlier'}));
+        scores.robustcov = sc;
+    end
+    
     % Local Outlier Factor
-    tic;
-    [~,tf, sc] = lof(data, ContaminationFraction=(1-alpha), ...
-        CategoricalPredictors=NameValueArgs.CategoricalPredictors);
-    t = toc;
-    fprintf("lof: %0.4f sec\n", t);
-    grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
-    grouphat(tf) = "outlier";
-    stats.lof = confusionstats(confusionmat(labels,grouphat, Order={'outlier' 'inlier'}));
-    scores.lof = sc;
+    if ismember('lof', NameValueArgs.Estimators)
+        tic;
+        [~,tf, sc] = lof(data, ContaminationFraction=(1-alpha), ...
+            CategoricalPredictors=NameValueArgs.CategoricalPredictors);
+        t = toc;
+        fprintf("lof: %0.4f sec\n", t);
+
+        grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
+        grouphat(tf) = "outlier";
+        stats.lof = confusionstats(confusionmat(labels,grouphat, Order={'outlier' 'inlier'}));
+        scores.lof = sc;
+    end
 
     % Isolation Forest
-    tic;
-    [~,tf, sc] = iforest(data, ContaminationFraction=(1-alpha), ...
-        CategoricalPredictors=NameValueArgs.CategoricalPredictors);
-    t = toc;
-    fprintf("iforest: %0.4f sec\n", t);
-    grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
-    grouphat(tf) = "outlier";
-    stats.iforest = confusionstats(confusionmat(labels,grouphat, Order={'outlier' 'inlier'}));
-    scores.iforest = sc;
+    if ismember('iforest', NameValueArgs.Estimators)
+        tic;
+        [~,tf, sc] = iforest(data, ContaminationFraction=(1-alpha), ...
+            CategoricalPredictors=NameValueArgs.CategoricalPredictors);
+        t = toc;
+        fprintf("iforest: %0.4f sec\n", t);
+        
+        grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
+        grouphat(tf) = "outlier";
+        stats.iforest = confusionstats(confusionmat(labels,grouphat, Order={'outlier' 'inlier'}));
+        scores.iforest = sc;
+    end
 
     % Random Robust Cut Forest
-    tic;
-    [~,tf,sc] = rrcforest(data, ContaminationFraction=(1-alpha), ...
-        CategoricalPredictors=NameValueArgs.CategoricalPredictors);
-    t = toc;
-    fprintf("rrcforest: %0.4f sec\n", t);
-    grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
-    grouphat(tf) = "outlier";
-    stats.rrcforest = confusionstats(confusionmat(labels,grouphat, Order={'outlier' 'inlier'}));
-    scores.rrcforest = sc;
+    if ismember('rrcforest', NameValueArgs.Estimators)
+        tic;
+        [~,tf,sc] = rrcforest(data, ContaminationFraction=(1-alpha), ...
+            CategoricalPredictors=NameValueArgs.CategoricalPredictors);
+        t = toc;
+        fprintf("rrcforest: %0.4f sec\n", t);
+        
+        grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
+        grouphat(tf) = "outlier";
+        stats.rrcforest = confusionstats(confusionmat(labels,grouphat, Order={'outlier' 'inlier'}));
+        scores.rrcforest = sc;
+    end
 
     % One-Class Support Vector Machine
-    tic;
-    [~,tf,sc] = ocsvm(data, ContaminationFraction=(1-alpha), ...
-        CategoricalPredictors=NameValueArgs.CategoricalPredictors);
-    t = toc;
-    fprintf("ocsvm: %0.4f sec\n", t);
-    grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
-    grouphat(tf) = "outlier";
-    stats.ocsvm = confusionstats(confusionmat(labels,grouphat, Order={'outlier' 'inlier'}));
-    scores.ocsvm = sc;
+    if ismember('ocsvm', NameValueArgs.Estimators)
+        tic;
+        [~,tf,sc] = ocsvm(data, ContaminationFraction=(1-alpha), ...
+            CategoricalPredictors=NameValueArgs.CategoricalPredictors);
+        t = toc;
+        fprintf("ocsvm: %0.4f sec\n", t);
+        
+        grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
+        grouphat(tf) = "outlier";
+        stats.ocsvm = confusionstats(confusionmat(labels,grouphat, Order={'outlier' 'inlier'}));
+        scores.ocsvm = sc;
+    end
 
     clear tf;
     clear sc;
 
-    % only print prcirves for contaminated datasets
+    % only print prcurves for contaminated datasets
     if numel(unique(labels)) == 2
         outlierRatio = sum(labels == "outlier")/numel(labels);
     
@@ -94,10 +119,13 @@ function result = evaluation(data, labels, alpha, solution, NameValueArgs)
         end
     end
 
+    s = stats.kMRCD;
+    
+    fn = fieldnames(stats);
+    for i=2:numel(fn)
+        s = vertcat(s, stats.(fn{i}));
+    end
 
-    result = struct2table( ...
-        vertcat(stats.kMRCD, stats.lof, ...
-                stats.iforest, stats.rrcforest, ...
-                stats.ocsvm), RowNames=fieldnames(stats));
+    result = struct2table(s, RowNames=fieldnames(stats));
 end
 
