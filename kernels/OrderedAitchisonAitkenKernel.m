0cb2406c7177c394a2bbc1d96159633f35314e75
Matthias Pichler <e1634256@student.tuwien.ac.at>
2023-12-15 22:36:47 +0100

    feat: add ordered kernel

---
+++ kernels/OrderedAitchisonAitkenKernel.m
@@ -0,0 +1,100 @@
+classdef OrderedAitchisonAitkenKernel < handle
+    %
+    %            n
+    % k(x,y) = PROD binom(c_i,|x_i-y_i|)*l_i^|x_i-y_i|*(1-l_i)^(c_i - |x_i-y_i|)
+    %           i=1
+    %
+    % c_i = number of categories for variable X_i
+
+    properties (Access = private)
+        categories (1,:) double
+    end
+    
+    properties (Access = public)
+        lambda (1,:) double {mustBeInRange(lambda,0,1)}
+    end
+
+    methods (Access = private)
+        function d = aitchisonaitkendist(this, ZI, ZJ)
+            arguments
+                this OrderedAitchisonAitkenKernel
+                ZI (1,:) double
+                ZJ double
+            end
+
+            l = this.lambda;
+            c = this.categories;
+            
+            % differences between x_i, y_i
+            s = abs(ZI - ZJ);
+
+            C = repmat(c, [height(ZJ), 1]);
+
+            % l^s * (1-l)^(c-s)
+            d = l.^(s).*(1-l).^(C-s);
+            
+            nck = ones(size(d));
+            for i=1:height(s)
+                for j = 1:width(s)
+                    nck(i,j) = nchoosek(c(j), s(i, j));
+                end
+            end
+            
+            d = nck .* d;
+
+            d = prod(d,2);
+        end
+    end
+    
+    methods (Static, Access = private)
+        function l = pluginbandwidth(x)
+            arguments
+                x double
+            end
+            
+            n = height(x);
+
+            [~, gr, gp] = cellfun(@groupcounts, num2cell(x, 1), UniformOutput=false);
+
+            c = cellfun(@length, gr);
+            pmf = cellfun(@(c)c/100, gp, UniformOutput=false);
+
+            numerator = n * cell2mat(cellfun(@(p){sum((1/numel(p)-p).^2)}, pmf));
+            denominator = cell2mat(cellfun(@(p){sum(p.*(1-p))}, pmf));
+
+            l = 1 - ((c-1)./c) ./ (1+(numerator./denominator));
+        end
+    end
+
+    methods (Access = public)
+        
+        function this = OrderedAitchisonAitkenKernel(x, NameValueArgs)
+            arguments
+                x double
+                NameValueArgs.lambda (1,:) double = OrderedAitchisonAitkenKernel.pluginbandwidth(x);
+            end
+            
+            g = cellfun(@groupcounts, num2cell(x, 1), UniformOutput=false);
+
+            this.categories = cellfun(@length, g);
+            
+            this.lambda = NameValueArgs.lambda;
+
+            disp(['OrderedAitchisonAitkenKernel: Lambda = ' mat2str(this.lambda)]);
+        end
+        
+        function K = compute(this, Xtrain, Xtest)
+            arguments
+                this OrderedAitchisonAitkenKernel
+                Xtrain double
+                Xtest double = Xtrain
+            end
+            
+            K = pdist2(Xtrain, Xtest, @this.aitchisonaitkendist);
+            
+            assert(size(K, 1)==size(Xtrain, 1));
+            assert(size(K, 2)==size(Xtest, 1));
+        end
+    end
+end
+
---
+++ kernels/OrderedAitchisonAitkenKernelTest.m
@@ -0,0 +1,136 @@
+classdef OrderedAitchisonAitkenKernelTest < matlab.unittest.TestCase
+    
+    methods(TestClassSetup)
+        % Shared setup for the entire test class
+    end
+    
+    methods(TestMethodSetup)
+        % Setup for each test
+    end
+    
+    methods(Test)
+        % Test methods
+        
+        function identity2(testCase)
+            l = 0.75;
+            expected = [[(1-l)^4,      4*l^2*(1-l)^2]
+                        [4*l^2*(1-l)^2,  (1-l)^4]];
+
+            x = eye(2);
+            kernel = OrderedAitchisonAitkenKernel(x, lambda=[l l]);
+
+            res = kernel.compute(x);
+
+            testCase.verifyEqual(res, expected);
+        end
+
+        function identity2lambda(testCase)
+            l1 = 0.75;
+            l2 = 0.5;
+            expected = [[(1-l1)^2*(1-l2)^2,         2*l1*(1-l1)*2*l2*(1-l2)]
+                        [2*l1*(1-l1)*2*l2*(1-l2),   (1-l1)^2*(1-l2)^2]];
+
+            x = eye(2);
+            kernel = OrderedAitchisonAitkenKernel(x, lambda=[l1 l2]);
+
+            res = kernel.compute(x);
+
+            testCase.verifyEqual(res, expected);
+        end
+
+        function identity3(testCase)
+            l = 0.75;
+            expected = [[(1-l)^6,       4*l^2*(1-l)^4,  4*l^2*(1-l)^4]
+                        [4*l^2*(1-l)^4, (1-l)^6,        4*l^2*(1-l)^4]
+                        [4*l^2*(1-l)^4, 4*l^2*(1-l)^4,  (1-l)^6]];
+
+            x = eye(3);
+            kernel = OrderedAitchisonAitkenKernel(x, lambda=[l l l]);
+
+            res = kernel.compute(x);
+
+            testCase.verifyEqual(res, expected);
+        end
+
+        function identity3autolambda(testCase)
+            l = 1 - 1/2 / (1+(3*(1/2-1/3)^2/(1/3*(1-1/3))));
+            
+            expected = [[(1-l)^6,       4*l^2*(1-l)^4,  4*l^2*(1-l)^4]
+                        [4*l^2*(1-l)^4, (1-l)^6,        4*l^2*(1-l)^4]
+                        [4*l^2*(1-l)^4, 4*l^2*(1-l)^4,  (1-l)^6]];
+
+            x = eye(3);
+            kernel = OrderedAitchisonAitkenKernel(x);
+
+            res = kernel.compute(x);
+
+            testCase.verifyEqual(res, expected, AbsTol=0.0001);
+        end
+
+        function identity3lambda(testCase)
+            l1 = 0.75;
+            l2 = 0.5;
+            l3 = 0.6;
+            expected = [[(1-l1)^2*(1-l2)^2*(1-l3)^2,        2*l1*(1-l1)*2*l2*(1-l2)*(1-l3)^2,   2*l1*(1-l1)*(1-l2)^2*2*l3*(1-l3)]
+                        [2*l1*(1-l1)*2*l2*(1-l2)*(1-l3)^2,  (1-l1)^2*(1-l2)^2*(1-l3)^2,         (1-l1)^2*2*l2*(1-l2)*2*l3*(1-l3)]
+                        [2*l1*(1-l1)*(1-l2)^2*2*l3*(1-l3),  (1-l1)^2*2*l2*(1-l2)*2*l3*(1-l3),   (1-l1)^2*(1-l2)^2*(1-l3)^2]];
+
+            x = eye(3);
+            kernel = OrderedAitchisonAitkenKernel(x, lambda=[l1 l2 l3]);
+
+            res = kernel.compute(x);
+
+            testCase.verifyEqual(res, expected);
+        end
+
+        function identity4(testCase)
+            l = 0.75;
+
+            expected = [[(1-l)^8,       4*l^2*(1-l)^6,  4*l^2*(1-l)^6,  4*l^2*(1-l)^6]
+                        [4*l^2*(1-l)^6, (1-l)^8,        4*l^2*(1-l)^6,  4*l^2*(1-l)^6]
+                        [4*l^2*(1-l)^6, 4*l^2*(1-l)^6,  (1-l)^8,        4*l^2*(1-l)^6]
+                        [4*l^2*(1-l)^6, 4*l^2*(1-l)^6,  4*l^2*(1-l)^6,  (1-l)^8]];
+
+            
+            x = eye(4);
+            kernel = OrderedAitchisonAitkenKernel(x, lambda=[l l l l]);
+
+            res = kernel.compute(x);
+
+            testCase.verifyEqual(res, expected);
+        end
+    
+        function tall(testCase)
+            l = 0.75;
+            expected = [[(1-l)^5,       6*l^2*(1-l)^3,  6*l^2*(1-l)^3,  3*l*(1-l)^4]
+                        [6*l^2*(1-l)^3, (1-l)^5,        3*l^2*(1-l)^3,  6*l^3*(1-l)^2]
+                        [6*l^2*(1-l)^3, 3*l^2*(1-l)^3,  (1-l)^5,        2*l*(1-l)^4]
+                        [3*l*(1-l)^4,   6*l^3*(1-l)^2,  2*l*(1-l)^4,    (1-l)^5]];
+
+            x = [[1, 0]
+                 [0, 1]
+                 [2, 1]
+                 [2, 0]];
+            kernel = OrderedAitchisonAitkenKernel(x, lambda=[l l]);
+
+            res = kernel.compute(x);
+
+            testCase.verifyEqual(res, expected, AbsTol=0.001);
+        end
+
+        function fat(testCase)
+            l = 0.75;
+            expected = [[(1-l)^5,       4*l^2*(1-l)^3]
+                        [4*l^2*(1-l)^3, (1-l)^5]];
+
+            x = [[1, 0, 0]
+                 [0, 1, 0]];
+            kernel = OrderedAitchisonAitkenKernel(x, lambda=[l l l]);
+
+            res = kernel.compute(x);
+
+            testCase.verifyEqual(res, expected);
+        end
+    end
+    
+end
