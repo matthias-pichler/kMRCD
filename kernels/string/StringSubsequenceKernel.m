28e5aa60aa8d299ab80b26fa32f20cfd7c9b6d51
Matthias Pichler <e1634256@student.tuwien.ac.at>
2023-12-22 19:22:20 +0100

    refactor: rewrite ssk in c++

--- kernels/string/StringSubsequenceKernel.m
+++ kernels/string/StringSubsequenceKernel.m
@@ -4,55 +4,6 @@ classdef StringSubsequenceKernel < handle
         lambda (1,1) double {mustBeInRange(lambda, 0, 1)} = 0.5
         maxSubsequence (1,1) double {mustBeInteger, mustBePositive} = 1
     end
-    
-    methods (Access = private)
-        function k = ssk(this, s, t)
-            arguments
-                this StringSubsequenceKernel
-                s (1,1) string
-                t (1,1) string
-            end
-
-            % to byte codes
-            s = double(char(s));
-            t = double(char(t));
-
-            k_prim = zeros(this.maxSubsequence, length(s), length(t));
-            k_prim(1,:,:) = 1;
-        
-            for i = 2:this.maxSubsequence
-                for sj = i:length(s)
-                    toret = 0;
-                    for tk = i:length(t)
-                        if s(sj-1) == t(tk-1)
-                            toret = this.lambda * (toret + this.lambda * k_prim(i-1, sj-1, tk-1));
-                        else
-                            toret = toret * this.lambda;
-                        end
-                        k_prim(i, sj, tk) = toret + this.lambda * k_prim(i, sj-1, tk);
-                    end
-                end
-            end
- 
-            k = 0;
-            for i = 1:this.maxSubsequence
-                for sj = i:length(s)
-                    tk_range = i:length(t);
-                    matches = (s(sj) == t(tk_range)); % Logical array of matches
-                    k_prim_slice = squeeze(k_prim(i, sj, tk_range))'; % Row vector
-                    k = k + sum(this.lambda^2 * k_prim_slice .* matches);
-                end
-            end
-        end
-
-        function d = sskdist(this, ZI, ZJ)
-            arguments
-                this StringSubsequenceKernel
-                ZI (1, 1) string
-                ZJ (:, 1) string
-            end
-        end
-    end
 
     methods (Access = public)
         
@@ -73,28 +24,7 @@ classdef StringSubsequenceKernel < handle
                 Xtest (:, 1) string = Xtrain
             end
             
-            [nTrain, ~] = size(Xtrain);
-            [nTest, ~] = size(Xtest);
-
-            K = zeros(nTrain, nTest);
-        
-            for i = 1:nTrain
-                for j = 1:nTest
-                    K(i, j) = this.ssk(Xtrain(i, :), Xtest(j, :));
-                end
-            end
-        
-            K_train = zeros(nTrain, 1);
-            K_test = zeros(nTest, 1);
-        
-            for i = 1:nTrain
-                K_train(i) = this.ssk(Xtrain(i, :), Xtrain(i, :));
-            end
-            for j = 1:nTest
-                K_test(j) = this.ssk(Xtest(j, :), Xtest(j, :));
-            end
-        
-            K = K ./ sqrt(repmat(K_test', height(K_train), 1) .* K_train);
+            K = ssk(Xtrain, Xtest, uint8(this.maxSubsequence), this.lambda);
 
             assert(size(K, 1)==size(Xtrain, 1));
             assert(size(K, 2)==size(Xtest, 1));
--- kernels/string/ssk.cpp
+++ kernels/string/ssk.cpp
@@ -1,104 +1,175 @@
 #include <vector>
 #include <cmath>
-#include <stdexcept>
-#include <iomanip>
-#include <iostream>
 
-// Kernel defined by Lodhi et al. (2002)
-double ssk(const std::string& s_, const std::string& t_, size_t n, double lbda) {
-    size_t lens = s_.length(), lent = t_.length();
-    std::vector<std::vector<std::vector<double>>> k_prim(n, std::vector<std::vector<double>>(lens, std::vector<double>(lent, 0)));
+// Mex wrapper
+#include "mex.hpp"
+#include "mexAdapter.hpp"
 
-    for (size_t i = 0; i < lens; ++i) {
-        for (size_t j = 0; j < lent; ++j) {
-            k_prim[0][i][j] = 1;
+class MexFunction : public matlab::mex::Function
+{
+public:
+    void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs)
+    {
+        this->checkArguments(outputs, inputs);
+
+        const matlab::data::StringArray Xtrain = inputs[0];
+        const matlab::data::StringArray Xtest = inputs[1];
+        const size_t n = inputs[2][0];
+        const double lambda = inputs[3][0];
+
+        const auto K = this->string_kernel(Xtrain, Xtest, n, lambda);
+
+        outputs[0] = std::move(K);
+    }
+
+private:
+    matlab::data::ArrayFactory factory;
+    std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = this->getEngine();
+
+    void checkArguments(matlab::mex::ArgumentList &outputs, matlab::mex::ArgumentList &inputs)
+    {
+        // Check number of inputs
+        if (inputs.size() != 4)
+        {
+            this->matlabPtr->feval(u"error",
+                                   0,
+                                   std::vector<matlab::data::Array>({this->factory.createScalar("Four inputs required")}));
+        }
+
+        // Check Xtrain argument: first input must be string (:,1)
+        if (inputs[0].getType() != matlab::data::ArrayType::MATLAB_STRING ||
+            inputs[0].getDimensions().at(1) != 1)
+        {
+            this->matlabPtr->feval(u"error",
+                                   0,
+                                   std::vector<matlab::data::Array>({this->factory.createScalar("'Xtrain' must be string array of size (:,1)")}));
+        }
+
+        // Check Xtest argument: second input must be string (:,1)
+        if (inputs[1].getType() != matlab::data::ArrayType::MATLAB_STRING ||
+            inputs[1].getDimensions().at(1) != 1)
+        {
+            this->matlabPtr->feval(u"error",
+                                   0,
+                                   std::vector<matlab::data::Array>({this->factory.createScalar("'Xtest' must be string array of size (:,1)")}));
+        }
+
+        // Check maxSubsequence argument: Third input must be scalar uint8
+        if (inputs[2].getType() != matlab::data::ArrayType::UINT8 ||
+            inputs[2].getNumberOfElements() != 1)
+        {
+            this->matlabPtr->feval(u"error",
+                                   0,
+                                   std::vector<matlab::data::Array>({this->factory.createScalar("'maxSubsequence' must be scalar uint8")}));
+        }
+
+        // Check lambda argument: Third input must be scalar double
+        if (inputs[3].getType() != matlab::data::ArrayType::DOUBLE ||
+            inputs[3].getNumberOfElements() != 1)
+        {
+            this->matlabPtr->feval(u"error",
+                                   0,
+                                   std::vector<matlab::data::Array>({this->factory.createScalar("'lambda' must be scalar double")}));
+        }
+
+        // Check number of outputs
+        if (outputs.size() > 1)
+        {
+            this->matlabPtr->feval(u"error",
+                                   0,
+                                   std::vector<matlab::data::Array>({this->factory.createScalar("Only one output is returned")}));
         }
     }
 
-    for (size_t i = 1; i < n; ++i) {
-        for (size_t sj = i; sj < lens; ++sj) {
-            double toret = 0.0;
-            for (size_t tk = i; tk < lent; ++tk) {
-                if (s_[sj-1] == t_[tk-1]) {
-                    toret = lbda * (toret + lbda * k_prim[i-1][sj-1][tk-1]);
-                } else {
-                    toret *= lbda;
+    matlab::data::TypedArray<double> string_kernel(const matlab::data::StringArray &xs, const matlab::data::StringArray &ys, size_t n, double lbda)
+    {
+        size_t lenxs = xs.getDimensions().at(0), lenys = ys.getDimensions().at(0);
+
+        if (lenxs == 0 || lenys == 0)
+        {
+            throw std::runtime_error("Input strings must not be empty.");
+        }
+
+        matlab::data::TypedArray<double> mat = this->factory.createArray<double>({lenxs, lenys});
+
+        for (size_t i = 0; i < lenxs; ++i)
+        {
+            for (size_t j = 0; j < lenys; ++j)
+            {
+                mat[i][j] = ssk(xs[i], ys[j], n, lbda);
+            }
+        }
+
+        matlab::data::TypedArray<double> mat_xs = this->factory.createArray<double>({lenxs}, {0.0});
+        matlab::data::TypedArray<double> mat_ys = this->factory.createArray<double>({lenys}, {0.0});
+
+        for (size_t i = 0; i < lenxs; ++i)
+        {
+            mat_xs[i] = ssk(xs[i], xs[i], n, lbda);
+        }
+        for (size_t j = 0; j < lenys; ++j)
+        {
+            mat_ys[j] = ssk(ys[j], ys[j], n, lbda);
+        }
+
+        for (size_t i = 0; i < lenxs; ++i)
+        {
+            for (size_t j = 0; j < lenys; ++j)
+            {
+                mat[i][j] /= sqrt(mat_xs[i] * mat_ys[j]);
+            }
+        }
+
+        return mat;
+    }
+
+    double ssk(const matlab::data::MATLABString &s_, const matlab::data::MATLABString &t_, size_t n, double lbda)
+    {
+        size_t lens = (*s_).length(), lent = (*t_).length();
+        std::vector<std::vector<std::vector<double>>> k_prim(n, std::vector<std::vector<double>>(lens, std::vector<double>(lent, 0)));
+
+        for (size_t i = 0; i < lens; ++i)
+        {
+            for (size_t j = 0; j < lent; ++j)
+            {
+                k_prim[0][i][j] = 1;
+            }
+        }
+
+        for (size_t i = 1; i < n; ++i)
+        {
+            for (size_t sj = i; sj < lens; ++sj)
+            {
+                double toret = 0.0;
+                for (size_t tk = i; tk < lent; ++tk)
+                {
+                    if ((*s_).at(sj - 1) == (*t_).at(tk - 1))
+                    {
+                        toret = lbda * (toret + lbda * k_prim[i - 1][sj - 1][tk - 1]);
+                    }
+                    else
+                    {
+                        toret *= lbda;
+                    }
+                    k_prim[i][sj][tk] = toret + lbda * k_prim[i][sj - 1][tk];
                 }
-                k_prim[i][sj][tk] = toret + lbda * k_prim[i][sj-1][tk];
             }
         }
-    }
 
-    double k = 0.0;
-    for (size_t i = 0; i < n; ++i) {
-        for (size_t sj = i; sj < lens; ++sj) {
-            for (size_t tk = i; tk < lent; ++tk) {
-                if (s_[sj] == t_[tk]) {
-                    k += lbda * lbda * k_prim[i][sj][tk];
+        double k = 0.0;
+        for (size_t i = 0; i < n; ++i)
+        {
+            for (size_t sj = i; sj < lens; ++sj)
+            {
+                for (size_t tk = i; tk < lent; ++tk)
+                {
+                    if ((*s_).at(sj) == (*t_).at(tk))
+                    {
+                        k += lbda * lbda * k_prim[i][sj][tk];
+                    }
                 }
             }
         }
+        return k;
     }
-    return k;
-}
-
-std::vector<std::vector<double>> string_kernel(const std::vector<std::string>& xs, const std::vector<std::string>& ys, size_t n, double lbda) {
-    size_t lenxs = xs.size(), lenys = ys.size();
-
-    if (lenxs == 0 || lenys == 0) {
-        throw std::runtime_error("Input strings must not be empty.");
-    }
-
-    std::vector<std::vector<double>> mat(lenxs, std::vector<double>(lenys, 0.0));
-
-    for (size_t i = 0; i < lenxs; ++i) {
-        for (size_t j = 0; j < lenys; ++j) {
-            mat[i][j] = ssk(xs[i], ys[j], n, lbda);
-        }
-    }
-
-    std::vector<double> mat_xs(lenxs, 0.0);
-    std::vector<double> mat_ys(lenys, 0.0);
-
-    for (size_t i = 0; i < lenxs; ++i) {
-        mat_xs[i] = ssk(xs[i], xs[i], n, lbda);
-    }
-    for (size_t j = 0; j < lenys; ++j) {
-        mat_ys[j] = ssk(ys[j], ys[j], n, lbda);
-    }
-
-    for (size_t i = 0; i < lenxs; ++i) {
-        for (size_t j = 0; j < lenys; ++j) {
-            mat[i][j] /= sqrt(mat_xs[i] * mat_ys[j]);
-        }
-    }
-
-    return mat;
-}
-
-// Example usage
-int main() {
-    std::vector<std::string> xs = {"cat", "car", "cart", "camp", "shard"};
-    std::vector<std::string> ys = {"a", "cd"};
-    size_t n = 2;
-    double lbda = 1.0;
-
-    std::vector<std::string> test = {"This is a very long string, just to test how fast this implementation of ssk is. It should look like the computation tooks no time, unless you're running this in a potato pc"};
-
-    try {
-        auto result = string_kernel(xs, ys, n, lbda);
-        // Output or process the result as needed
-        for (const auto& row : result) {
-            for (const auto& element : row) {
-                std::cout << std::fixed << std::setprecision(4) << std::setw(10) << element << " ";
-            }
-            std::cout << std::endl;
-        }
-
-        string_kernel(test, test, 30, 0.8f);
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
-
-    return 0;
-}
+};
