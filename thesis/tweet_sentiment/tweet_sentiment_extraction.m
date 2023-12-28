f336e2a4788109201da21b22a25d13baea709b4d
Matthias Pichler <e1634256@student.tuwien.ac.at>
2023-12-29 00:17:22 +0100

    run tweet sentiment extraction

--- thesis/tweet_sentiment/tweet_sentiment_extraction.m
+++ thesis/tweet_sentiment/tweet_sentiment_extraction.m
@@ -22,39 +22,41 @@ imageDir = fullfile(projectDir, 'images', datasetName);
 tableDir = fullfile(projectDir, 'tables', datasetName);
 datasetDir = fullfile(projectDir, 'datasets', datasetName);
 
-file = fullfile(datasetDir, [datasetName '_' modelName '.parquet']);
+datasetFile = fullfile(datasetDir, [datasetName '_' modelName '.parquet']);
 
 mkdir(imageDir, modelName);
 mkdir(tableDir, modelName);
 
-%% Load Data
+imageDir = fullfile(imageDir, modelName);
+tableDir = fullfile(tableDir, modelName);
 
-N = 1000;
-[eps20, eps20Labels] = generateSample(file, N, 0.2);
+%% Visualize
 
-%% e=0.2, a=0.7
+[embeddings, labels] = generateSample(datasetFile, 1000, 0.2);
 
-data = eps20;
-labels = eps20Labels;
-alpha = 0.7;
-
-Y = tsne(data, Distance="cosine");
+Y = tsne(embeddings, Distance="cosine");
 fig = figure(1);
 gscatter(Y(:,1), Y(:,2), labels);
 title("t-SNE Embeddings");
-saveas(fig,fullfile(imageDir, modelName, "e02_tsne.png"),'png');
+saveas(fig,fullfile(imageDir, "tsne.png"),'png');
 
 clear Y;
 
-kModel = AutoSphereRbfKernel(data);
-% kModel = AutoRbfKernel(data);
+%% Sample
+
+alpha = 0.7;
+N = 1000;
+
+kModel = AutoSphereRbfKernel(embeddings);
+% kModel = AutoRbfKernel(embeddings);
+
 poc = kMRCD(kModel); 
-solution = poc.runAlgorithm(data, alpha);
+solution = poc.runAlgorithm(embeddings, alpha);
 
 % h Subset
 hSubset = table(labels(solution.hsubsetIndices), VariableNames="label");
 hSubsetSummary = groupcounts(hSubset, "label");
-writetable(hSubsetSummary, fullfile(tableDir, modelName, "e02_h_subset.csv"));
+writetable(hSubsetSummary, fullfile(tableDir, "h_subset.csv"));
 
 clear hSubset hSubsetSummary;
 
@@ -66,38 +68,78 @@ cm = confusionmat(labels,grouphat);
 
 fig = figure(2);
 confusionchart(fig, cm, categories(labels));
-saveas(fig, fullfile(imageDir, modelName, 'e02_confusion_matrix.png'),'png');
+saveas(fig, fullfile(imageDir, 'confusion_matrix.png'),'png');
 
 clear cm grouphat;
 
 % Mahalanobis Distances
 fig = figure(3);
 mahalchart(labels, solution.rd, solution.cutoff);
-saveas(fig, fullfile(imageDir, modelName, 'e02_mahalanobis_distances.png'),'png');
+saveas(fig, fullfile(imageDir, 'mahalanobis_distances.png'),'png');
 
 % Comparison
 fig = figure(4);
-stats = evaluation(data, labels, alpha, solution, Estimators={'lof' 'iforest'});
-saveas(fig, fullfile(imageDir, modelName, 'e02_pr_curve.png'),'png');
-writetable(stats, fullfile(tableDir, modelName, "e02_comparison.csv"));
+stats = evaluation(embeddings, labels, alpha, solution, Estimators={'lof' 'iforest'});
+saveas(fig, fullfile(imageDir, 'pr_curve.png'),'png');
 
 clear stats;
-
 clear solution kModel alpha poc;
-clear data labels;
-%% Comparison
+clear embeddings labels;
 
-[eps0, eps0Labels] = generateSample(file, N, 0);
-[eps10, eps10Labels] = generateSample(file, N, 0.1);
-[eps30, eps30Labels] = generateSample(file, N, 0.3);
+set(0,'DefaultFigureVisible','off');
+%% Run a = 0.5, e = 0.1
 
-stats = [
-        runComparison(eps10, eps10Labels, 0.1, 0.5); runComparison(eps10, eps10Labels, 0.1, 0.75); runComparison(eps10, eps10Labels, 0.1, 0.9); ...
-        runComparison(eps20, eps20Labels, 0.2, 0.5); runComparison(eps20, eps20Labels, 0.2, 0.75); runComparison(eps20, eps20Labels, 0.2, 0.9); ...
-        runComparison(eps30, eps30Labels, 0.3, 0.5); runComparison(eps30, eps30Labels, 0.3, 0.75); runComparison(eps30, eps30Labels, 0.3, 0.9)
-        ];
+filepath = fullfile(tableDir, "simulation_a05_e01.csv");
+stats = runComparison(maxIterations=100, alpha=0.5, contamination=0.1, dataset=datasetFile, file=filepath);
 
-writetable(stats, fullfile(tableDir, modelName, 'comparison.csv'));
+fig = figure(5);
+boxchart(stats.name, stats.aucpr, MarkerStyle="none");
+saveas(fig, fullfile(imageDir, 'boxplot_a05_e01.png'),'png');
+
+%% Run a = 0.75, e = 0.1
+
+filepath = fullfile(tableDir, "simulation_a075_e01.csv");
+stats = runComparison(maxIterations=100, alpha=0.75, contamination=0.1, dataset=datasetFile, file=filepath);
+
+fig = figure(6);
+boxchart(stats.name, stats.aucpr, MarkerStyle="none");
+saveas(fig, fullfile(imageDir, 'boxplot_a075_e01.png'),'png');
+
+%% Run a = 0.9, e = 0.1
+
+filepath = fullfile(tableDir, "simulation_a09_e01.csv");
+stats = runComparison(maxIterations=100, alpha=0.9, contamination=0.1, dataset=datasetFile, file=filepath);
+
+fig = figure(7);
+boxchart(stats.name, stats.aucpr, MarkerStyle="none");
+saveas(fig, fullfile(imageDir, 'boxplot_a09_e01.png'),'png');
+
+%% Run a = 0.5, e = 0.2
+
+filepath = fullfile(tableDir, "simulation_a05_e02.csv");
+stats = runComparison(maxIterations=100, alpha=0.5, contamination=0.2, dataset=datasetFile, file=filepath);
+
+fig = figure(8);
+boxchart(stats.name, stats.aucpr, MarkerStyle="none");
+saveas(fig, fullfile(imageDir, 'boxplot_a05_e02.png'),'png');
+
+%% Run a = 0.75, e = 0.2
+
+filepath = fullfile(tableDir, "simulation_a075_e02.csv");
+stats = runComparison(maxIterations=100, alpha=0.75, contamination=0.2, dataset=datasetFile, file=filepath);
+
+fig = figure(9);
+boxchart(stats.name, stats.aucpr, MarkerStyle="none");
+saveas(fig, fullfile(imageDir, 'boxplot_a075_e02.png'),'png');
+
+%% Run a = 0.5, e = 0.3
+
+filepath = fullfile(tableDir, "simulation_a05_e03.csv");
+stats = runComparison(maxIterations=100, alpha=0.5, contamination=0.3, dataset=datasetFile, file=filepath);
+
+fig = figure(10);
+boxchart(stats.name, stats.aucpr, MarkerStyle="none");
+saveas(fig, fullfile(imageDir, 'boxplot_a05_e03.png'),'png');
 
 %% Functions
 
@@ -131,23 +173,50 @@ function [embeddings,labels] = generateSample(filepath, sampleSize, contaminatio
     labels = labels(perm, :);
 end
 
-function stats = runComparison(data, labels, outlierContamination, robustness)
+function stats = runComparison(NameValueArgs)
     arguments
-        data double {mustBeNonempty}
-        labels categorical {mustBeNonempty}
-        outlierContamination (1,1) double {mustBeInRange(outlierContamination,0,1)}
-        robustness (1,1) double {mustBeInRange(robustness,0.5,1)}
+        NameValueArgs.dataset (1,1) string {mustBeFile}
+        NameValueArgs.maxIterations (1,1) double {mustBeInteger, mustBePositive} = 100
+        NameValueArgs.alpha (1,1) double {mustBeInRange(NameValueArgs.alpha, 0.5, 1)}
+        NameValueArgs.contamination (1,1) double {mustBeInRange(NameValueArgs.contamination, 0, 0.5)}
+        NameValueArgs.file (1,1) string
+    end
+
+    alpha = NameValueArgs.alpha;
+    iter = NameValueArgs.maxIterations;
+    file = NameValueArgs.file;
+
+    start = 1;
+
+    if isfile(file)
+        opts = detectImportOptions(file);
+        opts = setvartype(opts, 'double');
+        opts = setvartype(opts,'name', 'categorical');
+        results = readtable(file, opts);
+        start = max(results.iteration) + 1;
+    end
+
+    for i = start:iter
+        fprintf("Iteration: %d\n", i);
+
+        [data, labels] = generateSample(NameValueArgs.dataset, 1000, NameValueArgs.contamination);
+        
+        kModel = AutoSphereRbfKernel(data);
+        
+        poc = kMRCD(kModel); 
+        solution = poc.runAlgorithm(data, alpha);
+    
+        e = evaluation(data, labels, alpha, solution, Estimators={'lof' 'iforest'});
+    
+        results = vertcat(e('kMRCD',:), e('lof',:), e('iforest', :));
+        results.name = ["kMRCD";"lof";"iforest"];
+        results.iteration = repmat(i, 3, 1);
+        
+        writetable(results, file, WriteMode="append");
     end
     
-    kModel = AutoSphereRbfKernel(data);
-    poc = kMRCD(kModel); 
-    solution = poc.runAlgorithm(data, robustness);
-
-    stats = evaluation(data, labels, robustness, solution, Estimators={'lof' 'iforest'});
-
-    names = string(stats.Properties.RowNames);
-    e = repmat(outlierContamination,size(names));
-    a = repmat(robustness,size(names));
-    stats = [table(e,a,names, VariableNames={'e' 'a' 'Name'}), stats];
-    stats.Properties.RowNames = {};
+    opts = detectImportOptions(file);
+    opts = setvartype(opts, 'double');
+    opts = setvartype(opts,'name', 'categorical');
+    stats = readtable(file, opts);
 end
