c9d3bb774b23109477128364589e2b86859ff919
Matthias Pichler <e1634256@student.tuwien.ac.at>
2023-12-30 01:28:13 +0100

    feat: run multiple kernel sims

--- thesis/normal_simulation/normal_simulation_kernels.m
+++ thesis/normal_simulation/normal_simulation_kernels.m
@@ -1,4 +1,4 @@
-%% Discretized Normal Distribution
+%% Discretized Normal Distribution Kernels
 
 % Setup
 clc;
@@ -17,124 +17,147 @@ tableDir = fullfile(projectDir, 'tables', datasetName);
 mkdir(imageDir);
 mkdir(tableDir);
 
-%% Generate Data
-
-contamination = 0.2;
-numCategories = 5;
-dimensions = 32;
-N = 1000;
-
-ndm = NewDataModel(ALYZCorrelationType(), ClusterContamination());
-[x, ~, ~,idxOutliers] = ndm.generateDataset(N, dimensions, contamination, 20);        
-
-unlabeledData = cell2mat(cellfun(@(X)discretize(X, numCategories), num2cell(x, 1), UniformOutput=false));
-
-labels = categorical(repmat("inlier", [N 1]), {'inlier' 'outlier'});
-labels(idxOutliers) = "outlier";
-
-clear x;
-
-%% Visualize
-
-Y = tsne(unlabeledData);
-fig = figure(1);
-gscatter(Y(:,1), Y(:,2), labels);
-title("t-SNE Embeddings");
-saveas(fig,fullfile(imageDir, "e02_tsne.png"),'png');
-
-clear Y;
-
 %% Run
 
+set(0,'DefaultFigureVisible','off');
+
+file = fullfile(tableDir, "simulation_a07_e02.csv");
+
 alpha = 0.7;
-s = struct();
 
-%% Linear
-kModel = LinKernel();
-solution = kMRCD(kModel).runAlgorithm(unlabeledData, alpha);
+iter = 100;
+start = 1;
 
-s(1).kernel = "Linear";
-s(1).solution = solution;
-
-%% RBF
-kModel = AutoRbfKernel(unlabeledData);
-solution = kMRCD(kModel).runAlgorithm(unlabeledData, alpha);
-
-s(2).kernel = "RBF";
-s(2).solution = solution;
-
-%% Dirac
-
-kModel = DiracKernel();
-solution = kMRCD(kModel).runAlgorithm(unlabeledData, alpha);
-s(3).kernel = "Dirac";
-s(3).solution = solution;
-
-%% k1
-kModel = K1Kernel(unlabeledData);
-solution = kMRCD(kModel).runAlgorithm(unlabeledData, alpha);
-s(4).kernel = "k1";
-s(4).solution = solution;
-
-%% m3
-kModel = M3Kernel(unlabeledData);
-solution = kMRCD(kModel).runAlgorithm(unlabeledData, alpha);
-s(5).kernel = "m3";
-s(5).solution = solution;
-
-%% Aitchison-Aitken
-kModel = AitchisonAitkenKernel(unlabeledData);
-solution = kMRCD(kModel).runAlgorithm(unlabeledData, alpha);
-s(6).kernel = "Aitchison-Aitken";
-s(6).solution = solution;
-
-%% Li-Racin
-kModel = LiRacinKernel(unlabeledData);
-solution = kMRCD(kModel).runAlgorithm(unlabeledData, alpha);
-s(7).kernel = "Li-Racin";
-s(7).solution = solution;
-
-%% Summary
-
-for i = 1:numel(s)
-    grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
-    grouphat(s(i).solution.flaggedOutlierIndices) = "outlier";
-    cm = confusionmat(labels,grouphat);
-    stats = confusionstats(confusionmat(labels,grouphat, Order={'outlier' 'inlier'}));
-
-    s(i).accuracy = stats.accuracy;
-    s(i).precision = stats.precision;
-    s(i).sensitivity = stats.sensitivity;
-    s(i).specificity = stats.specificity;
-    s(i).f1Score = stats.f1Score;
+if isfile(file)
+    opts = detectImportOptions(file);
+    opts = setvartype(opts, 'double');
+    opts = setvartype(opts,'kernel', 'categorical');
+    results = readtable(file, opts);
+    start = max(results.iteration) + 1;
 end
 
-%% PR-Curve
+for i = start:iter
+    fprintf("Iteration: %d\n", i);
 
-outlierRatio = sum(labels == "outlier")/numel(labels);
-colors = jet(numel(s));
+    [data, labels] = generateData(size=1000, contamination=0.2, dimensions=30, categories=5);
+    
+    s = struct();
+    %%% Linear
+    kModel = LinKernel();
+    solution = kMRCD(kModel).runAlgorithm(data, alpha);
+    
+    s(1).kernel = "Linear";
+    s(1).solution = solution;
+    
+    %%% RBF
+    kModel = AutoRbfKernel(data);
+    solution = kMRCD(kModel).runAlgorithm(data, alpha);
+    
+    s(2).kernel = "RBF";
+    s(2).solution = solution;
+    
+    %%% Dirac
+    kModel = DiracKernel();
+    solution = kMRCD(kModel).runAlgorithm(data, alpha);
+    s(3).kernel = "Dirac";
+    s(3).solution = solution;
+    
+    %%% k1
+    kModel = K1Kernel(data);
+    solution = kMRCD(kModel).runAlgorithm(data, alpha);
+    s(4).kernel = "k1";
+    s(4).solution = solution;
+    
+    %%% m3
+    kModel = M3Kernel(data);
+    solution = kMRCD(kModel).runAlgorithm(data, alpha);
+    s(5).kernel = "m3";
+    s(5).solution = solution;
+    
+    %%% Aitchison-Aitken
+    kModel = AitchisonAitkenKernel(data);
+    solution = kMRCD(kModel).runAlgorithm(data, alpha);
+    s(6).kernel = "Aitchison-Aitken";
+    s(6).solution = solution;
+    
+    %%% Li-Racin
+    kModel = LiRacinKernel(data);
+    solution = kMRCD(kModel).runAlgorithm(data, alpha);
+    s(7).kernel = "Li-Racin";
+    s(7).solution = solution;
 
-fig = figure(2);
-hold on;
+    %%% Wang-Ryzin
+    kModel = WangRyzinKernel(data, lambda=repmat(0.01, 1, width(data)));
+    solution = kMRCD(kModel).runAlgorithm(data, alpha);
+    s(8).kernel = "Wang-Ryzin";
+    s(8).solution = solution;
+    
+    %%% Ordered Aitchison-Aitken
+    kModel = OrderedAitchisonAitkenKernel(data);
+    solution = kMRCD(kModel).runAlgorithm(data, alpha);
+    s(9).kernel = "Ordered Aitchison-Aitken";
+    s(9).solution = solution;
+    
+    %%% Ordered Li-Racin
+    kModel = OrderedLiRacinKernel(data, lambda=repmat(0.01, 1, width(data)));
+    solution = kMRCD(kModel).runAlgorithm(data, alpha);
+    s(10).kernel = "Ordered Li-Racin";
+    s(10).solution = solution;
 
-xlabel("Recall");
-ylabel("Precision");
-title("Precision-Recall Curve");
+    colors = jet(numel(s));
 
-for i = 1:numel(s)
-    scores = s(i).solution.rd;
-    auc = prcurve(labels,scores,'outlier',DisplayName=s(i).kernel, Color=colors(i,:));
-    s(i).aucpr = auc;
+    %%% Summary
+    for j = 1:numel(s)
+        grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
+        grouphat(s(j).solution.flaggedOutlierIndices) = "outlier";
+        cm = confusionmat(labels,grouphat, Order={'outlier' 'inlier'});
+        stats = confusionstats(cm);
+    
+        s(j).accuracy = stats.accuracy;
+        s(j).precision = stats.precision;
+        s(j).sensitivity = stats.sensitivity;
+        s(j).specificity = stats.specificity;
+        s(j).f1Score = stats.f1Score;
+
+        scores = s(j).solution.rd;
+        auc = prcurve(labels,scores,'outlier',DisplayName=s(j).kernel, Color=colors(j,:));
+        s(j).aucpr = auc;
+    end
+
+    results = struct2table(rmfield(s, "solution"));
+    results.iteration = repmat(i, numel(s), 1);
+    writetable(results, file, WriteMode="append");
 end
 
-yline(outlierRatio, LineStyle="--", ...
-      DisplayName=sprintf("No Skill Classifier (AUC=%0.4f)", outlierRatio));
-legend;
-hold off;
-saveas(fig,fullfile(imageDir, "pr_curve.png"),'png');
+opts = detectImportOptions(file);
+opts = setvartype(opts, 'double');
+opts = setvartype(opts,'name', 'categorical');
+stats = readtable(file, opts);
 
-% Comparison
-stats = struct2table(s);
-writetable(stats, fullfile(tableDir, "comparison.csv"));
+fig = figure(1);
+boxchart(stats.kernel, stats.aucpr, MarkerStyle="none");
+saveas(fig, fullfile(imageDir, 'boxplot_a07_e02.png'),'png');
 
-clear s;
+%% Functions
+
+function [data, labels] = generateData(NameValueArgs)
+    arguments
+        NameValueArgs.size (1,1) double {mustBeInteger, mustBePositive}
+        NameValueArgs.contamination (1,1) double {mustBeInRange(NameValueArgs.contamination, 0, 0.5)}
+        NameValueArgs.dimensions (1,1) double {mustBeInteger, mustBePositive}
+        NameValueArgs.categories (1,1) double {mustBeInteger, mustBePositive}
+    end
+
+    contamination = NameValueArgs.contamination;
+    numCategories = NameValueArgs.categories;
+    dimensions = NameValueArgs.dimensions;
+    N = NameValueArgs.size;
+    
+    ndm = NewDataModel(ALYZCorrelationType(), ClusterContamination());
+    [x, ~, ~,idxOutliers] = ndm.generateDataset(N, dimensions, contamination, 20);        
+    
+    data = cell2mat(cellfun(@(X)discretize(X, numCategories), num2cell(x, 1), UniformOutput=false));
+    
+    labels = categorical(repmat("inlier", [N 1]), {'inlier' 'outlier'});
+    labels(idxOutliers) = "outlier";
+end
