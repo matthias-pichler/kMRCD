7deba53ce1b585500fbc741bde3e90e28ab16478
Matthias Pichler <e1634256@student.tuwien.ac.at>
2024-09-09 09:45:13 +0200

    refactor: update shakespear trump

--- thesis/shakespear_trump/shakespear_trump_kernels.m
+++ thesis/shakespear_trump/shakespear_trump_kernels.m
@@ -5,13 +5,14 @@
 clc;
 clear all;
 close all;
-
 rng(1634256, "twister");
 
 fileDir = fileparts(which(mfilename));
 projectDir = fileparts(fileparts(fileDir));
 
-modelName = 'all-mpnet-base-v2';
+% modelName = 'GIST-small-Embedding-v0';
+modelName = 'nomic-embed-text-v1.5';
+% modelName = 'all-mpnet-base-v2';
 % modelName = 'bge-large-en-v1.5';
 % modelName = 'bge-small-en-v1.5';
 % modelName = 'all-MiniLM-L6-v2';
@@ -19,52 +20,37 @@ modelName = 'all-mpnet-base-v2';
 datasetName = 'shakespear_trump';
 % datasetName = 'shakespear_trump_cleaned';
 
+matryoshkaDim = 512;
+
 imageDir = fullfile(projectDir, 'images', [datasetName '_kernels']);
 tableDir = fullfile(projectDir, 'tables', [datasetName '_kernels']);
 datasetDir = fullfile(projectDir, 'datasets', datasetName);
 
-file = fullfile(datasetDir, [datasetName '_' modelName '.parquet']);
+datasetFile = fullfile(datasetDir, [datasetName '_' modelName '.parquet']);
 
-mkdir(imageDir, modelName);
-mkdir(tableDir, modelName);
+folderName = modelName;
+
+if ~isnan(matryoshkaDim)
+    folderName = [modelName '-' int2str(matryoshkaDim)];
+end
+
+mkdir(imageDir, folderName);
+mkdir(tableDir, folderName);
+
+imageDir = fullfile(imageDir, folderName);
+tableDir = fullfile(tableDir, folderName);
 
 %% Load Data
 
-sampleSize = 1000;
-outlierContamination= 0.2;
-
-rawData = parquetread(file, SelectedVariableNames=["text", "author", "embedding"]);
-rawData.author = categorical(rawData.author);
-
-shakespearIndices = find(rawData.author == "shakespear");
-trumpIndices = find(rawData.author == "trump");
-
-numShakespearSamples = round(outlierContamination * sampleSize);
-numTrumpSamples = sampleSize - numShakespearSamples;
-
-shakespearSampleIndices = datasample(shakespearIndices, numShakespearSamples, Replace=false);
-trumpSampleIndices = datasample(trumpIndices, numTrumpSamples, Replace=false);
-data = rawData(sort(cat(1, shakespearSampleIndices, trumpSampleIndices)),:);
-
-embeddings = cell2mat(cellfun(@transpose,data.embedding, UniformOutput=false));
-labels = renamecats(data.author, {'trump' 'shakespear'}, {'inlier' 'outlier'});
-
-perm = randperm(height(embeddings));
-text = data.text(perm, :);
-embeddings = embeddings(perm, :);
-labels = labels(perm, :);
-
-clear numShakespearSamples numTrumpSamples;
-clear shakespearSampleIndices trumpSampleIndices;
-clear rawData perm;
+[embeddings, labels] = generateSample(datasetFile, 1000, 0.2, matryoshkaDim=matryoshkaDim);
 
 %% Visualize
 
-Y = tsne(data, Distance="cosine");
+Y = tsne(embeddings, Distance="cosine");
 fig = figure(1);
 gscatter(Y(:,1), Y(:,2), labels);
 title("t-SNE Embeddings");
-saveas(fig,fullfile(imageDir, modelName, "e02_tsne.png"),'png');
+saveas(fig,fullfile(imageDir, "tsne.png"),'png');
 
 clear Y;
 
@@ -77,31 +63,50 @@ s = struct();
 kModel = AutoRbfKernel(embeddings);
 solution = kMRCD(kModel).runAlgorithm(embeddings, alpha);
 
-s(1).kernel = "RBF";
+s(1).kernel = "AutoRbfKernel";
 s(1).solution = solution;
+
+%% Scaled RBF
+kModel = ScaledAutoRbfKernel(embeddings);
+solution = kMRCD(kModel).runAlgorithm(embeddings, alpha);
+
+s(2).kernel = "ScaledAutoRbfKernel";
+s(2).solution = solution;
 
 %% Sphere-RBF
 kModel = AutoSphereRbfKernel(embeddings);
 solution = kMRCD(kModel).runAlgorithm(embeddings, alpha);
 
-s(2).kernel = "Sphere-RBF";
-s(2).solution = solution;
+s(3).kernel = "AutoSphereRbfKernel";
+s(3).solution = solution;
+
+%% Scale-Sphere-RBF
+kModel = ScaledAutoSphereRbfKernel(embeddings);
+solution = kMRCD(kModel).runAlgorithm(embeddings, alpha);
+
+s(4).kernel = "ScaledAutoSphereRbfKernel";
+s(4).solution = solution;
 
 %% String-Subsequence
-kModel = StringSubsequenceKernel(text);
-solution = kMRCD(kModel).runAlgorithm(text, alpha);
+% kModel = StringSubsequenceKernel(lambda=0.6, maxSubsequence=15);
+% solution = kMRCD(kModel).runAlgorithm(text, alpha);
 
-s(3).kernel = "SSK";
-s(3).solution = solution;
+% s(5).kernel = "StringSubsequenceKernel";
+% s(5).solution = solution;
 
 %% Summary
 
 for i = 1:numel(s)
+    fig = figure();
+    
+    mahalchart(labels, s(i).solution.rd, s(i).solution.cutoff);
+    saveas(fig, fullfile(imageDir, sprintf("mahalanobis_distances_%s.png", s(i).kernel)),'png');
+    
     grouphat = categorical(repmat("inlier", size(labels)), categories(labels));
     grouphat(s(i).solution.flaggedOutlierIndices) = "outlier";
     cm = confusionmat(labels,grouphat);
     stats = confusionstats(confusionmat(labels,grouphat, Order={'outlier' 'inlier'}));
-
+    
     s(i).accuracy = stats.accuracy;
     s(i).precision = stats.precision;
     s(i).sensitivity = stats.sensitivity;
@@ -128,7 +133,7 @@ for i = 1:numel(s)
 end
 
 yline(outlierRatio, LineStyle="--", ...
-      DisplayName=sprintf("No Skill Classifier (AUC=%0.4f)", outlierRatio));
+    DisplayName=sprintf("No Skill Classifier (AUC=%0.4f)", outlierRatio));
 legend(Location="southwest");
 hold off;
 saveas(fig,fullfile(imageDir, "pr_curve.png"),'png');
@@ -137,4 +142,38 @@ saveas(fig,fullfile(imageDir, "pr_curve.png"),'png');
 stats = struct2table(s);
 writetable(stats, fullfile(tableDir, "comparison.csv"));
 
-clear s;
+%% Functions
+
+function [embeddings,labels] = generateSample(filepath, sampleSize, outlierContamination, NameValueArgs)
+    arguments
+        filepath (1,1) string {mustBeFile}
+        sampleSize (1,1) double {mustBeInteger, mustBePositive}
+        outlierContamination (1,1) double {mustBeInRange(outlierContamination,0,1)}
+        NameValueArgs.matryoshkaDim (1,1) double {mustBePositive, mustBeInteger} = Inf
+    end
+    
+    embeddingVar = "embedding";
+    if ~isnan(NameValueArgs.matryoshkaDim) && isfinite(NameValueArgs.matryoshkaDim)
+        embeddingVar = ['embedding_' int2str(NameValueArgs.matryoshkaDim)];
+    end
+    
+    rawData = parquetread(filepath, SelectedVariableNames=["text", "author", embeddingVar]);
+    rawData.author = categorical(rawData.author);
+    
+    shakespearIndices = find(rawData.author == "shakespear");
+    trumpIndices = find(rawData.author == "trump");
+    
+    numShakespearSamples = round(outlierContamination * sampleSize);
+    numTrumpSamples = sampleSize - numShakespearSamples;
+    
+    shakespearSampleIndices = datasample(shakespearIndices, numShakespearSamples, Replace=false);
+    trumpSampleIndices = datasample(trumpIndices, numTrumpSamples, Replace=false);
+    data = rawData(sort(cat(1, shakespearSampleIndices, trumpSampleIndices)),:);
+    
+    embeddings = cell2mat(cellfun(@transpose,data{:, embeddingVar}, UniformOutput=false));
+    labels = renamecats(data.author, {'trump' 'shakespear'}, {'inlier' 'outlier'});
+    
+    perm = randperm(height(embeddings));
+    embeddings = embeddings(perm, :);
+    labels = labels(perm, :);
+end
