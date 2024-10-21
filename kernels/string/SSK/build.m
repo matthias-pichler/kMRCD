f3b5d0620aa8cfde3687974cdf6a5079efa6d052
Matthias Pichler <matthias@pichler.co.com>
2024-10-21 23:22:38 +0200

    chore: play around with build flags

--- kernels/string/SSK/build.m
+++ kernels/string/SSK/build.m
@@ -3,8 +3,27 @@ fileDir = fileparts(which(mfilename));
 inputFile = fullfile(fileDir, 'ssk.cpp');
 outDir = fileDir;
 
+cxxFlags = [
+    "CXXFLAGS=$CXXFLAGS",...
+    "-std=c++17", ... % To enable newer features
+    "-Xclang", ...
+    "-fopenmp", "-I/opt/homebrew/opt/libomp/include", ... % include OpenMP
+    "-fopenmp-simd", "-fno-split-stack", ... % faster OpenMP
+    "-O3", ... % optimize aggressively
+    "-march=native", "-funroll-loops", "-ftree-vectorize", ... % SIMD
+    "-ffast-math", ... % faster floats
+    "-flto", ... % Link-Time Optimization
+    ];
+
+ldFlags = [
+    "LDFLAGS=$LDFLAGS", ... 
+    "-L/opt/homebrew/opt/libomp/lib", "-lomp",... % include OpenMP
+    "-flto", ... % Link-Time Optimization
+    ];
+
 mex(inputFile, ...
     '-v',...
     '-outdir', outDir, ...
-    'CXXFLAGS=$CXXFLAGS -std=c++17 -Xclang -fopenmp -I/opt/homebrew/opt/libomp/include -lomp',...
-    'LDFLAGS=$LDFLAGS -L/opt/homebrew/opt/libomp/lib -lomp');
+    strjoin(cxxFlags),...
+    strjoin(ldFlags) ...
+    );
